d0f37348e2f81021851e051e32ee7c6ebdb7dbaf
Ogulcan Gumussoy <ogulcangumussoy@gmail.com>
2019-04-27 16:53:00 +0300

    Initial Commit

---
+++ app/src/main/java/teknoyel/com/mobil_asistanim/HomeActivity.kt
@@ -0,0 +1,12 @@
+package teknoyel.com.mobil_asistanim
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
